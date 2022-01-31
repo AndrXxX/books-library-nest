4e18fe884c5b1372694b1b3bda782f42ad2b0161
Andrew Garetov <andrew1gar@yandex.ru>
2022-01-31 21:15:33 +0300

    006 Реализация BooksModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BooksModule } from "src/modules/books/books.module";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const modules = [
+  BooksModule,
+];
+
 @Module({
-  imports: [],
+  imports: [
+    ...modules,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
