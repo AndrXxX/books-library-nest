289ebe6244e85d08fad42b98b9dbc26c390fc798
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-30 21:09:21 +0300

    #008 Добавлен book.schema

---
+++ books-library/src/modules/books/mongo.schemas/book.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BookDocument = HydratedDocument<Book>;
+
+@Schema()
+export class Book {
+  @Prop( { required: [true, 'Не указано название'] })
+  title: string;
+
+  @Prop( { default: '' })
+  description: string;
+
+  @Prop( { default: [] })
+  authors: string[];
+
+  @Prop( { default: '' })
+  favorite: string;
+
+  @Prop( { default: '' })
+  fileCover: string;
+
+  @Prop( { default: '' })
+  fileName: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
