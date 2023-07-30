76aff7bae3cd73fa8b4c9a141b176cab13b23752
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-30 20:53:28 +0300

    #008 Подключение mongoose

--- books-library/src/app.module.ts
+++ books-library/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { BooksModule } from "src/modules/books/books.module";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config';
 
 const modules = [
   BooksModule,
+  MongooseModule.forRoot(config.dbUrl)
 ];
 
 @Module({
