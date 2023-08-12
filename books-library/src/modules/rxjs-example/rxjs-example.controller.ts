8897d977c38eaa0ac885119b42063a83ad0ab96b
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-12 21:36:49 +0300

    #009 Реализация получения результатов

--- books-library/src/modules/rxjs-example/rxjs-example.controller.ts
+++ books-library/src/modules/rxjs-example/rxjs-example.controller.ts
@@ -3,11 +3,12 @@ import { RxjsExampleService } from "./rxjs-example.service";
 
 @Controller('rxjs-example')
 export class RxjsExampleController {
-  constructor(private rxjsService: RxjsExampleService) {
-  }
+  constructor(
+    private rxjsService: RxjsExampleService)
+  {}
 
-  @Get('gitlab/:text')
-  async gitlab(@Param() { text }) {
-    return await this.rxjsService.searchGitlab(text);
+  @Get('repositories/:text')
+  async repositories(@Param() { text }) {
+    return await this.rxjsService.searchRepo(text);
   }
 }
--- books-library/src/modules/rxjs-example/rxjs-example.service.ts
+++ books-library/src/modules/rxjs-example/rxjs-example.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios from "axios";
+import { from, map, merge, take } from "rxjs";
 
 @Injectable()
 export class RxjsExampleService {
+  private readonly githubUrl = 'https://api.github.com/search/repositories?q=';
+  private readonly gitlabUrl = 'https://gitlab.com/api/v4/projects?search=';
 
-  public async searchGitlab(text: string): Promise<any> {
-    return { text: text };
+  public async searchRepo(text: string): Promise<any> {
+    const firstSource$ = from((await axios.get(`${this.githubUrl}${text}`)).data.items)
+      .pipe(map((item: any) => ({ title: item.name, url: item.html_url, source: "github" })))
+      .pipe(take(5));
+    const secondSource$ = from((await axios.get(`${this.gitlabUrl}${text}`)).data)
+      .pipe(map((item: any) => ({ title: item.name, url: item.web_url, source: "gitlab" })))
+      .pipe(take(5))
+    const common$ = merge(firstSource$, secondSource$);
+    const items = [];
+    common$.subscribe((value) => items.push(value));
+    return items;
   }
 
 }
