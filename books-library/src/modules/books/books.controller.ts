7261077d250e04562f0056d3223729057ed18788
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-13 21:45:25 +0300

    #010 Добавлен DtoValidationPipe

--- books-library/src/modules/books/books.controller.ts
+++ books-library/src/modules/books/books.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { iBook } from "src/interfaces/book.interface";
+import { DtoValidationPipe } from "src/validators/dto.validation.pipe";
 import { IdValidationPipe } from "src/validators/id.validation.pipe";
 import { BooksService } from "./books.service";
 import { iCreateBookDto } from "./interfaces/book-create.interface";
-import { iUpdateBookDto } from "./interfaces/book-update.interface";
+import { UpdateBookDto } from "./interfaces/book-update.interface";
 
 @Controller('books')
 export class BooksController {
@@ -23,7 +24,7 @@ export class BooksController {
   @Put(':id')
   public update(
     @Param('id', IdValidationPipe) id: string,
-    @Body() body: iUpdateBookDto,
+    @Body(DtoValidationPipe) body: UpdateBookDto,
   ) {
     return this.booksService.update(id as string, body);
   }
--- books-library/src/modules/books/interfaces/book-update.interface.ts
+++ books-library/src/modules/books/interfaces/book-update.interface.ts
@@ -1,8 +1,22 @@
-export interface iUpdateBookDto {
+import { IsArray, IsDefined, IsOptional, IsString, Length } from "class-validator";
+
+export class UpdateBookDto {
+  @Length(10, 20) @IsDefined() @IsString()
   title: string;
+
+  @IsOptional() @IsString()
   description?: string;
+
+  @IsOptional()
+  @IsArray()
   authors?: string[];
+
+  @IsOptional() @IsString()
   favorite?: string;
+
+  @IsOptional() @IsString()
   fileCover?: string;
+
+  @IsOptional() @IsString()
   fileName?: string;
 }
