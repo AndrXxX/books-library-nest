dc1d7a8516c6e96a1de8860c11d5d83a50ed5f5b
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-13 21:57:30 +0300

    #010 Добавлен UnifiedExceptionFilter

--- books-library/src/main.ts
+++ books-library/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { FormatResponseInterceptorInterceptor } from "src/app.format-response.interceptor";
+import { UnifiedExceptionFilter } from "src/filters/unified.exception.filter";
 import { AppModule } from './app.module';
 import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new FormatResponseInterceptorInterceptor)
+  app.useGlobalFilters(new UnifiedExceptionFilter());
+  app.useGlobalInterceptors(new FormatResponseInterceptorInterceptor());
   await app.listen(config.port);
 }
 bootstrap();
