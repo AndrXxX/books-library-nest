33f3f6265b60597507d6dfed4fc93eedce7ba6dd
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-19 23:34:39 +0300

    #011 Добавлен GenerateHashService

--- books-library/src/modules/auth/auth.module.ts
+++ books-library/src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import config from 'src/config';
+import { GenerateHashService } from "src/modules/auth/generate-hash.service";
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -16,7 +17,7 @@ import { LocalStrategy } from "./strategies/local.strategy";
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService, JwtStrategy, LocalStrategy],
-  exports: [AuthService],
+  providers: [AuthService, JwtStrategy, LocalStrategy, GenerateHashService],
+  exports: [AuthService, GenerateHashService],
 })
 export class AuthModule {}
