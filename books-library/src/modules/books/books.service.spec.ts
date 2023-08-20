5093d5fcc39ab26a548896af95db74e56c8c0803
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-20 17:22:34 +0300

    #012 Добавлен books.controller.spec.ts

--- books-library/src/modules/books/books.service.spec.ts
+++ books-library/src/modules/books/books.service.spec.ts
@@ -1,6 +1,6 @@
 import { getModelToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from '@nestjs/testing';
-import { Book, BookDocument } from "src/modules/books/mongo.schemas/book.schema";
+import { Book } from "src/modules/books/mongo.schemas/book.schema";
 import { BooksService } from './books.service';
 
 describe("BooksService", () => {
@@ -12,13 +12,7 @@ describe("BooksService", () => {
         BooksService,
         {
           provide: getModelToken(Book.name),
-          useFactory: () => ({
-            findAll: jest.fn(),
-            save: jest.fn(),
-            find: jest.fn(),
-            findOneAndUpdate: jest.fn(),
-            findOneAndRemove: jest.fn(),
-          }),
+          useFactory: () => ({}),
         }
       ],
       exports: [BooksService],
@@ -27,11 +21,7 @@ describe("BooksService", () => {
     booksService = await app.get<BooksService>(BooksService);
   });
 
-  describe("findAll", () => {
-    it("should return an array of Books", async () => {
-      const result: BookDocument[] = [new Book() as BookDocument];
-      jest.spyOn(booksService, "findAll").mockImplementation(async () => result);
-      expect(await booksService.findAll()).toBe(result);
-    });
+  it('should be defined', () => {
+    expect(booksService).toBeDefined();
   });
 });
