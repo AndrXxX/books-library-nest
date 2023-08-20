dce20bcac50f4eaef386d02f66e06553af7be46a
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-21 00:31:01 +0300

    #016 Переименована папка

--- books-library/src/modules/book-comments/book-comments.service.ts
+++ books-library/src/modules/book-comments/book-comments.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { BookComment, BookCommentDocument } from "src/modules/book-comments/mongo.schemas/book-comment.schema";
-import { CreateBookCommentDto } from "./interfaces/book-comment-create.interface";
+import { CreateBookCommentDto } from "./dto/book-comment-create.dto";
 
 @Injectable()
 export class BookCommentsService {
