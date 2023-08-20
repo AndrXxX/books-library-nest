e1ccb396446bf3f485bdeaf3b64b5c7c93af1fd7
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-21 00:16:37 +0300

    #016 Добавлен BookCommentsModule

--- books-library/src/app.module.ts
+++ books-library/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config';
 import { AuthModule } from "./modules/auth/auth.module";
+import { BookCommentsModule } from "./modules/book-comments/book-comments.module";
 import { BooksModule } from "./modules/books/books.module";
 import { RxjsExampleModule } from "./modules/rxjs-example/rxjs-example.module";
 import { UsersModule } from "./modules/users/users.module";
@@ -14,6 +15,7 @@ const modules = [
   RxjsExampleModule,
   UsersModule,
   AuthModule,
+  BookCommentsModule,
 ];
 
 @Module({
