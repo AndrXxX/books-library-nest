18efdfbe2010f1925c03fa7d86904dfe8c0a9ba9
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-21 00:51:25 +0300

    #016 Использование WsExceptionFilter и WsDtoValidationPipe

--- books-library/src/app.gateway.ts
+++ books-library/src/app.gateway.ts
@@ -1,10 +1,13 @@
+import { UseFilters } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { WsExceptionFilter } from "src/filters/ws.exception.filter";
 import { CreateBookCommentDto } from "src/modules/book-comments/dto/book-comment-create.dto";
-import { DtoValidationPipe } from "src/validators/dto.validation.pipe";
 import { IdValidationPipe } from "src/validators/id.validation.pipe";
+import { WsDtoValidationPipe } from "src/validators/ws.dto.validation.pipe";
 import { BookCommentsService } from "./modules/book-comments/book-comments.service";
 
+@UseFilters(new WsExceptionFilter)
 @WebSocketGateway({ cors: true })
 export class AppGateway {
   constructor(
@@ -20,7 +23,7 @@ export class AppGateway {
   }
 
   @SubscribeMessage('addComment')
-  async addComment(@MessageBody(DtoValidationPipe) createCommentDto: CreateBookCommentDto) {
+  async addComment(@MessageBody(WsDtoValidationPipe) createCommentDto: CreateBookCommentDto) {
     return await this.commentsService.create(createCommentDto);
   }
 }
