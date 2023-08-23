2ce9f59398b16ced8035e1fd3fb743e37169f645
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-23 18:00:45 +0300

    #014 Обновлены Endpoint

--- yandex-cloud-functions/index.js
+++ yandex-cloud-functions/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const serverless = require('serverless-http');
+const comicsStore = require('store/ComicsStore');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/api/characters', (req, res) => {
-  res.json([]);
+  res.json(comicsStore.getAll());
 });
 
 app.get('/api/character', (req, res) => {
   const id = req.query.id;
-  res.json([]);
+  const item = comicsStore.get(id);
+  if (item) {
+    return res.json(item);
+  }
+  res.code(404).json(null);
 });
 
 module.exports.handler = serverless(app);
