a9c52064b742429f14b337e074fec7493b2e0fbe
Andrew Garetov <andrew1gar@yandex.ru>
2023-08-24 16:26:17 +0300

    #015 Вынос части логики в yandex-auth

--- yandex-passport/index.js
+++ yandex-passport/index.js
@@ -1,30 +1,11 @@
 const express = require('express');
 const passport = require('passport');
-const YandexStrategy = require('passport-yandex').Strategy;
 const config = require('./config');
 const rootRouter = require('./routes/root');
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
-
-passport.use(new YandexStrategy({
-    clientID: config.yandexClientId,
-    clientSecret: config.yandexClientSecret,
-    callbackURL: config.yandexCallbackUrl,
-  },
-  (accessToken, refreshToken, profile, done) => {
-    process.nextTick(() => {
-      return done(null, profile);
-    });
-  }
-));
+const yandexAuth = require('./boot/yandex-auth');
 
 const app = express();
+yandexAuth();
 app.use(require('cookie-parser')());
 app.use(require('express-session')({ secret: config.cookieSecret }));
 
